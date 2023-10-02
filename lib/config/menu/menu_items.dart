bec058d321498e80a14cea1d31bf2df69e9d2afe
omarmontoys <omarmontoyaromero@gmail.com>
2023-10-02 10:34:56 -0600

    home screen implementado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 
-class MenuItem {
+class MenuItems {
   final String title;
   final String description;
   final IconData icon;
   final String link;
-  MenuItem(
+  const MenuItems(
       {required this.title,
       required this.description,
       required this.icon,
       required this.link});
 }
 
-List<MenuItem> appMenuItems = <MenuItem>[
-  MenuItem(
+const appMenuItems = <MenuItems>[
+  MenuItems(
       title: 'Botones',
       description: 'Varios botones en lutter',
       icon: Icons.smart_button_outlined,
       link: '/'),
-  MenuItem(
+  MenuItems(
       title: 'Tarjetas',
       description: 'Un contenedor estilizado',
       icon: Icons.credit_card,
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,46 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material3'),
       ),
+      body: _HomeView(),
     );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    appMenuItems;
+    return ListView.builder(
+        itemCount: appMenuItems.length,
+        itemBuilder: (context, index) {
+          final menuItem = appMenuItems[index];
+          return _CustomListTitle(menuItem: menuItem);
+        });
+  }
+}
+
+class _CustomListTitle extends StatelessWidget {
+  const _CustomListTitle({
+    required this.menuItem,
+  });
+
+  final MenuItems menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+        leading: Icon(
+          menuItem.icon,
+          color: colors.primary,
+        ),
+        trailing: Icon(Icons.arrow_forward_ios_outlined, color: colors.primary),
+        title: Text(menuItem.title),
+        subtitle: Text(menuItem.description),
+        onTap: () {
+          //TODO: navegar a otra pantalla.
+        });
   }
 }
